abda99d0d96833f8046dbab40f7da4b9c889eaeb
rickyjohnwhu <143750528+rickyjohnwhu@users.noreply.github.com>
2023-12-31 18:08:50 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -39,3 +39,19 @@ def counts_to_WD(data1, data2, pca, bounds):
     counts2 = prob_hist(pca_data2, bounds)
 
     return wasserstein_distance(counts1, counts2)
+
+def FE_hist(data, bounds, binw=0.1, binner=None):
+
+    if binner is None:
+        binner = (np.arange(bounds[0], bounds[1] + binw, binw),
+                    np.arange(bounds[2], bounds[3] + binw, binw))
+
+    counts, xedges, yedges = np.histogram2d(data[:,0], data[:,1],
+                                           bins=binner, density=True)
+    prob = counts + 1e-3
+    G = -np.log(prob)
+    G[G == np.inf] = -1
+    G[G == -1] = max(G.ravel())
+    G -= min(G.ravel())
+    
+    return G, xedges, yedges
