1f5d73e2af2e160b78f5c0d783982bfbd1533f53
Richard John <rjohn123@login-2.zaratan.umd.edu>
2024-01-11 17:33:56 -0500

    working as notebook

--- nfhelp.py
+++ nfhelp.py
@@ -4,7 +4,7 @@ import normflows as nf
 import time
 import matplotlib.pyplot as plt
 import pprint
-from utils import counts_to_KLD, counts_to_WD
+import utils
 from torch.utils.data import Dataset, TensorDataset, DataLoader, random_split
 from scipy.stats import wasserstein_distance
 from sklearn.decomposition import PCA
@@ -12,22 +12,22 @@ from matplotlib import pyplot as plt
 from torch import nn
 from tqdm import tqdm
 
-def preprocess_samples(filepath, split, batch_size, device):
+def preprocess_samples(filepath, split, batch_size, dataset_limiter):
 
   with open(filepath, 'r') as f:
     rawdata = np.loadtxt(f)
 
-  rawdata = rawdata[:]
+  rawdata = rawdata[:dataset_limiter]
 
-  alldata = torch.from_numpy(rawdata).to(device)
+  alldata = torch.from_numpy(rawdata)
 
   tensor_dataset = torch.utils.data.TensorDataset(alldata)
 
   train, valid, test = random_split(tensor_dataset, split, generator=torch.Generator().manual_seed(42))
 
-  train_np = train[:][0].to('cpu').numpy()
-  valid_np = valid[:][0].to('cpu').numpy()
-  test_np = test[:][0].to('cpu').numpy()
+  train_np = train[:][0].numpy()
+  valid_np = valid[:][0].numpy()
+  test_np = test[:][0].numpy()
 
   train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
 
@@ -47,9 +47,9 @@ def init_model(dimensionality, num_layers):
 
   return model
 
-def train_epoch(loader, model):
+def train_epoch(loader, model, lr):
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-5)
 
     #for x in tqdm(loader):
     for x in loader:
@@ -78,16 +78,16 @@ def sampler(model, num_samples):
   #print(f"Generated {num_samples} samples in {delta} seconds at a rate of {num_samples/delta} samples per second.")
   return gendata, num_samples/delta
 
-def train_and_sample(model, loader, valid_np, test_np, pca, bounds):
-  
+def train_and_sample(model, loader, valid_np, test_np, pca, bounds, lr):
+
   valid_KLD_hist = np.array([])
   valid_WD_hist = np.array([])
-  
+
   while valid_KLD_hist.shape[0] < 5 or np.mean(valid_KLD_hist[-3:] - valid_KLD_hist[-4:-1]) < 0:
-    train_epoch(loader, model)
-    
+    train_epoch(loader, model, lr)
+
     model.eval()
-    
+
     generated_samples = sampler(model, len(valid_np))[0]
     valid_KLD = utils.counts_to_KLD(generated_samples, valid_np, pca, bounds)
     valid_KLD_hist = np.append(valid_KLD_hist, valid_KLD)
@@ -95,14 +95,13 @@ def train_and_sample(model, loader, valid_np, test_np, pca, bounds):
     valid_WD_hist = np.append(valid_WD_hist, valid_WD)
     #print('KLD history:', valid_KLD_hist)
     #print('WD history:', valid_WD_hist)
-  
+
   generated_testing, speed = sampler(model, len(test_np))
   final_KLD_score = utils.counts_to_KLD(generated_testing, test_np, pca, bounds)
   final_WD_score = utils.counts_to_WD(generated_testing, test_np, pca, bounds)
-  iterations = len(valid_KLD_hist)
 
-  info = {'Training data amount': len(loader.dataset.indices), 'Learnable parameters': params, 'Iterations': iterations, 
-          'Speed (samples/s)': speed, 'Final KLD': final_KLD_score, 'Final WD': final_WD_score}
+  info = {'Architecture': 'NF', 'Training data amount': len(loader.dataset.indices), 'Learnable parameters': utils.count_parameters(model), 'Iterations': len(valid_KLD_hist),
+          'Speed (samples/s)': speed, 'Final KLD': final_KLD_score, 'Final WD': final_WD_score, 'Dimensions': len(generated_testing[0])}
 
   return generated_testing, info
 
